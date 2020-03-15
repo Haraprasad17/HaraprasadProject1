52caf265ccafd7b5b291f5277ac460d073fd910a
HARAPRASAD <HARAPRASAD@LAPTOP-3HQJ46KJ>
2020-03-15 08:32:28 +0530

    First Commit

---
+++ spring-boot-db-messageresource/target/classes/data/data.sql
@@ -0,0 +1,73 @@
+
+INSERT INTO languages (locale, messagekey,messagecontent) VALUES
+('en', 'home.welcome','Welcome'),
+('en', 'home.info','This page is displayed in English.'),
+('en', 'home.changelanguage','Supported languages : '),
+('en', 'home.lang.en','English'),
+('en', 'home.lang.de','German'),
+('en', 'home.lang.zh','Chinese'),
+
+('de', 'home.welcome','Welcome'),
+('de', 'home.info','Diese Seite wird in deutscher Sprache angezeigt.'),
+('de', 'home.changelanguage','Unterstützte Sprachen : '),
+('de', 'home.lang.en','Englisch'),
+('de', 'home.lang.de','Deutsch'),
+('de', 'home.lang.zh','Chinesisch'),
+
+('zh', 'home.welcome','Welcome'),
+('zh', 'home.info','此頁面以中文顯示.'),
+('zh', 'home.changelanguage','支持的語言  : '),
+('zh', 'home.lang.en','英語'),
+('zh', 'home.lang.de','德語'),
+('zh', 'home.lang.zh','普通話')
+;
+
+
+
+
+INSERT INTO languages (locale, messagekey,messagecontent) VALUES
+('en', 'label.firstname','FirstName'),
+('en', 'label.lastname','LastName'),
+('en', 'label.email','Email: '),
+('en', 'label.gender','Gender: '),
+('en', 'label.fathersname','FatherName:'),
+
+('en', 'label.lang.en','English'),
+('en', 'label.lang.fr','French'),
+('en', 'label.lang.sw','Swahili'),
+('en', 'label.lang.kin','Kinyarwanda'),
+
+('fr', 'label.firstname','Prénom'),
+('fr', 'label.lastname','Nom de famille'),
+('fr', 'label.email','Email:'),
+('fr', 'label.gender','Le sexe'),
+('fr', 'label.fathersname','Le nom du père'),
+
+
+('kin', 'label.firstname','Izina ryambere'),
+('kin', 'label.lastname','Izina ryanyuma'),
+('kin', 'label.email','Imeri:'),
+('kin', 'label.gender','Uburinganire'),
+('kin', 'label.fathersname','Izina rya Data'),
+
+
+('sw', 'label.firstname','Jina la kwanza'),
+('sw', 'label.lastname','Jina la familia'),
+('sw', 'label.email','Barua pepe :'),
+('sw', 'label.gender','Jinsia'),
+('sw', 'label.fathersname','Jina la baba')
+
+;
+
+INSERT INTO `rrr`.`languages` (`id`, `messagecontent`, `messagekey`, `locale`) VALUES ('25', 'Create your Account. It\'s free and only takes a minute', 'label.line', 'en');
+INSERT INTO `rrr`.`languages` (`id`, `messagecontent`, `messagekey`, `locale`) VALUES ('26', 'Créez votre compte. C\'est gratuit et ne prend qu\'une minute', 'label.line', 'fr');
+INSERT INTO `rrr`.`languages` (`id`, `messagecontent`, `messagekey`, `locale`) VALUES ('27', 'Kora Konti yawe. Nubuntu kandi bifata umunota umwe gusa', 'label.line', 'kin');
+INSERT INTO `rrr`.`languages` (`id`, `messagecontent`, `messagekey`, `locale`) VALUES ('28', 'Unda Akaunti yako. Ni bure na inachukua dakika moja', 'label.line', 'sw');
+INSERT INTO `rrr`.`languages` (`id`, `messagecontent`, `messagekey`, `locale`) VALUES ('29', 'Register', 'label.register', 'en');
+INSERT INTO `rrr`.`languages` (`id`, `messagecontent`, `messagekey`, `locale`) VALUES ('30', 'S\'inscrire', 'label.register', 'fr');
+INSERT INTO `rrr`.`languages` (`id`, `messagecontent`, `messagekey`, `locale`) VALUES ('31', 'Iyandikishe', 'label.register', 'kin');
+INSERT INTO `rrr`.`languages` (`id`, `messagecontent`, `messagekey`, `locale`) VALUES ('32', 'Jiandikishe', 'label.register', 'sw');
+INSERT INTO `rrr`.`languages` (`id`, `messagecontent`, `messagekey`, `locale`) VALUES ('33', 'Register here', 'label.button', 'en');
+INSERT INTO `rrr`.`languages` (`id`, `messagecontent`, `messagekey`, `locale`) VALUES ('34', 'Inscrivez-vous ici', 'label.button', 'fr');
+INSERT INTO `rrr`.`languages` (`id`, `messagecontent`, `messagekey`, `locale`) VALUES ('35', 'Iyandikishe hano', 'label.button', 'kin');
+INSERT INTO `rrr`.`languages` (`id`, `messagecontent`, `messagekey`, `locale`) VALUES ('36', 'Jiandikishe hapa', 'label.button', 'sw');
