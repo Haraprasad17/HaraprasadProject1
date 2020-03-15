2938955176003b90b318733000df4b3c8c4d8d10
HARAPRASAD <HARAPRASAD@LAPTOP-3HQJ46KJ>
2020-03-16 00:44:37 +0530

    updated

--- ARegistrationForm/src/main/java/com/csm/web/controller/PageController.java
+++ ARegistrationForm/src/main/java/com/csm/web/controller/PageController.java
@@ -1,12 +1,20 @@
 package com.csm.web.controller;
 
+import java.util.Date;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.csm.web.entity.FieldmasterEntity;
 import com.csm.web.entity.FinalEntity;
 import com.csm.web.repository.FieldmasterRepository;
 import com.csm.web.repository.FinalRepository;
@@ -51,8 +59,37 @@ public class PageController {
 		@RequestMapping("/editFieldmaster/{fieldID}")
 		public  ModelAndView Edit(@PathVariable("fieldID") int fieldID) {
 			ModelAndView mv =new ModelAndView("editFieldmaster");
-					mv.addObject("English",finalrepo.English());
+			mv.addObject("FieldmasterEntity", fieldrepo.findById(fieldID).orElse(new FieldmasterEntity()));
+		/*
+		 * mv.addObject("languagemasterData", langrepo.findAll());
+		 * mv.addObject("webpagemasterData", webrepo.findAll());
+		 * mv.addObject("finaltabledata",finalrepo.findAll());
+		 */
 			return mv;
+		}
+		
+		//now update 
+		@PostMapping("updatefieldmaster/{fieldID}")
+		public String updateUser(@PathVariable("fieldID") int fieldID, @Valid FieldmasterEntity fieldmasterEntity, 
+		  BindingResult result, Model model) {
+		    if (result.hasErrors()) {
+		    	fieldmasterEntity.setFieldID(fieldID);
+		    	
+		        return "editFieldmaster";
+		    }
+		    FieldmasterEntity f = fieldrepo.findById(fieldID).orElse(new FieldmasterEntity() );
+		    fieldmasterEntity.setUpdatedOn(new Date());
+		    fieldmasterEntity.setCreatedBy(1);
+		    fieldmasterEntity.setUpdatedBy(1);
+		    fieldmasterEntity.setCreatedOn(f.getCreatedOn());
+		    
+		    
+		    fieldrepo.save(fieldmasterEntity);
+		/*
+		 * model.addAttribute("fieldmasterEntitys", userRepository.findAll()); return
+		 * "index";
+		 */
+		    return"redirect:/viewpage";
 		}
 	
 	
